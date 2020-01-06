78d1779323852b94c6e91a517bb951991e041b79
Freddy Suarez <freddy.armando.suarez@gmail.com>
2020-01-05 22:40:55 -0300

    Updates

---
+++ precache-manifest.0e2693d86fd31e7f30853da002f2fddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ada504336ef827f389439e625de941d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7702c942a740043c15b8",
+    "url": "/react-gh-pages/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "89bef3aafde4c916d9d0",
+    "url": "/react-gh-pages/static/js/2.dcac16f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.dcac16f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7702c942a740043c15b8",
+    "url": "/react-gh-pages/static/js/main.b7c37a3a.chunk.js"
+  },
+  {
+    "revision": "488a3268d74ab2564c7c",
+    "url": "/react-gh-pages/static/js/runtime-main.31d90d75.js"
+  }
+]);
